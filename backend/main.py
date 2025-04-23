4eb60f66e90c857d0c4aedc408c66f553c087352
lswitaj <lukasz.ynd@orderbird.com>
2025-04-23 19:10:30 +0200

    Frontend: Add Vite project setup with React and Tailwind CSS Frontend and Backend communication established

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
-from logging import log
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 import os
 import random
 import json
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace "*" with specific origins in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def main():
@@ -14,13 +23,13 @@ async def main():
 async def health():
     return {"status": "ok"}
 
-@app.get("/topics")
+@app.get("/topic")
 async def gettopic():
     try:
         file_path = os.path.join(os.path.dirname(__file__), 'topics.json')
         with open(file_path, 'r') as file:
             topics = json.load(file)
-            randomTopic = random.choice(topics)["name"]
-            return {"message": randomTopic}
+            randomTopic = random.choice(topics)
+            return JSONResponse(randomTopic)
     except Exception as e:
         return {"error": str(e)}
